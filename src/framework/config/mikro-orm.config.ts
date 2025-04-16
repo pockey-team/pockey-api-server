ca384d019f45b7cf1bfaa78da90de9dd759ad844
seonkyo <tjs0466@naver.com>
2025-04-17 01:52:03 +0900

    misc: update db port to literal

--- src/framework/config/mikro-orm.config.ts
+++ src/framework/config/mikro-orm.config.ts
@@ -14,7 +14,7 @@ export const mikroOrmConfig: Options = defineConfig({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
+  port: 3306,
   driver: MySqlDriver,
   metadataProvider: TsMorphMetadataProvider,
   debug: !isProd,
