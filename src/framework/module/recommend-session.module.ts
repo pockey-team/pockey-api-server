2e2d4dd903654afd3ae18fbfd678e5613a58626e
seonkyo <tjs0466@naver.com>
2025-05-10 20:22:04 +0900

    fix: common question entity setting

--- src/framework/module/recommend-session.module.ts
+++ src/framework/module/recommend-session.module.ts
@@ -1,10 +1,13 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 
+import { CommonQuestionDbEntity } from '../../adapter/db/common-question.entity';
 import { CommonQuestionGateway } from '../../adapter/db/common-question.gateway';
 import { RecommendSessionController } from '../../adapter/web/recommend-session.controller';
 import { RecommendSessionService } from '../../application/service/recommend.service';
 
 @Module({
+  imports: [MikroOrmModule.forFeature([CommonQuestionDbEntity])],
   controllers: [RecommendSessionController],
   providers: [
     {
