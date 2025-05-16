7bd7d78108440e781d84bd90d06505d4fbf14b11
Park Hyewon <db1648@naver.com>
2025-05-16 17:31:14 +0900

    feat: 위시리스트 서비스 단위 테스트 및 예외 케이스 추가

--- src/framework/module/wishlist.module.ts
+++ src/framework/module/wishlist.module.ts
@@ -6,8 +6,10 @@ import { WishlistGateway } from 'src/adapter/db/wishlist.gateway';
 import { WishlistController } from 'src/adapter/web/wishlist.controller';
 import { WishlistService } from 'src/application/service/Wishlist.service';
 
+import { AuthGuardModule } from './auth.guard.module';
+
 @Module({
-  imports: [MikroOrmModule.forFeature([WishlistDbEntity, ProductDbEntity])],
+  imports: [MikroOrmModule.forFeature([WishlistDbEntity, ProductDbEntity]), AuthGuardModule],
   controllers: [WishlistController],
   providers: [
     WishlistService,
