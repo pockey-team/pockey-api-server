31ce07512e48ba927bb5e8adb1baf5ad968caf2e
Park Hyewon <db1648@naver.com>
2025-05-16 15:57:24 +0900

    refactor: jwtAuthGuard 전역 등록 해제 및 특정 API에만 적용하도록 수정

--- src/application/service/user.service.ts
+++ src/application/service/user.service.ts
@@ -2,14 +2,11 @@ import { Inject, Injectable } from '@nestjs/common';
 
 import { User } from '../../domain/user';
 import { UserUseCase } from '../port/in/user/UserUseCase';
-import { UserDbCommandPort } from '../port/out/UserDbCommandPort';
 import { UserDbQueryPort } from '../port/out/UserDbQueryPort';
 
 @Injectable()
 export class UserService implements UserUseCase {
   constructor(
-    @Inject('UserGateway')
-    private readonly userDbCommandPort: UserDbCommandPort,
     @Inject('UserGateway')
     private readonly userDbQueryPort: UserDbQueryPort,
   ) {}
