6dd84309f39dc84861b3c9f9fa12c47a6a17732e
Park Hyewon <db1648@naver.com>
2025-05-19 15:10:43 +0900

    refactor: removeList 비즈니스 로직에서 검증 수행

--- src/application/port/out/WishlistDbCommandPort.ts
+++ src/application/port/out/WishlistDbCommandPort.ts
@@ -2,5 +2,5 @@ import { AddWishlistCommand } from '../in/wishlist/WishlistUseCase';
 
 export interface WishlistDbCommandPort {
   addWishlist(command: AddWishlistCommand): Promise<void>;
-  removeWishlist(wishlistId: number, userId: number): Promise<void>;
+  removeWishlist(id: number): Promise<void>;
 }
